a00b4b0d3556e3f329ba5e4e5b0083ec5517995a
Shay Dadosh <shay127@gmail.com>
2020-12-29 21:18:04 +0200

    MediaMetadataRetriever can fail

--- imagepicker/src/main/java/com/esafirm/imagepicker/helper/ImagePickerUtils.java
+++ imagepicker/src/main/java/com/esafirm/imagepicker/helper/ImagePickerUtils.java
@@ -110,17 +110,22 @@ public class ImagePickerUtils {
     }
 
     public static String getVideoDurationLabel(Context context, File file) {
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        retriever.setDataSource(context, Uri.fromFile(file));
-        Long duration = Long.parseLong(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
-        retriever.release();
-        long second = (duration / 1000) % 60;
-        long minute = (duration / (1000 * 60)) % 60;
-        long hour = (duration / (1000 * 60 * 60)) % 24;
-        if (hour > 0) {
-            return String.format("%02d:%02d:%02d", hour, minute, second);
-        } else {
-            return String.format("%02d:%02d", minute, second);
+        try {
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(context, Uri.fromFile(file));
+            Long duration = Long.parseLong(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            retriever.release();
+            long second = (duration / 1000) % 60;
+            long minute = (duration / (1000 * 60)) % 60;
+            long hour = (duration / (1000 * 60 * 60)) % 24;
+            if (hour > 0) {
+                return String.format("%02d:%02d:%02d", hour, minute, second);
+            } else {
+                return String.format("%02d:%02d", minute, second);
+            }
+        } catch (RuntimeException ex) {
+            // something went wrong with the file, ignore it and continue
+            return "n/a";
         }
     }
 
